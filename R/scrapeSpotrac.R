1a25ee431823918fd2489eaa50759b4a4841f447
B. Harrison <87046186+b-harr@users.noreply.github.com>
2024-10-25 07:56:15 -0500

    Add files via upload

--- R/scrapeSpotrac.R
+++ R/scrapeSpotrac.R
@@ -1,18 +1,17 @@
-# Scrape Links - Spotrac
-
+# Load libraries
 library(dplyr)
 library(rvest)
 library(XML)
 library(stringr)
 library(janitor)
 
-# Set Spotrac Team IDs
+# Set links to scrape
 spotrac_link_ids <- c("atlanta-hawks", "brooklyn-nets", "boston-celtics", "charlotte-hornets", "cleveland-cavaliers", "chicago-bulls", "dallas-mavericks", "denver-nuggets", "detroit-pistons", "golden-state-warriors", "houston-rockets", "indiana-pacers", "la-clippers", "los-angeles-lakers", "memphis-grizzlies", "miami-heat", "milwaukee-bucks", "minnesota-timberwolves", "new-york-knicks", "new-orleans-pelicans", "oklahoma-city-thunder", "orlando-magic", "philadelphia-76ers", "phoenix-suns", "portland-trail-blazers", "san-antonio-spurs", "sacramento-kings", "toronto-raptors", "utah-jazz", "washington-wizards")
 years <- c(2024:2028)
 links <- paste0("https://www.spotrac.com/nba/", spotrac_link_ids, "/cap/_/year/")
-spotrac_links <- {}
-n <- 0
+spotrac_links <- vector()
 
+n <- 0
 for(i in 1:length(years)){
   for(j in 1:length(links)){
     n <- n + 1
@@ -20,45 +19,78 @@ for(i in 1:length(years)){
   }
 }
 
-# Create Function to Scrape Links
-scrape_spotrac_links <- function(link){
-  webpage <- read_html(link)
-  player_links <- webpage %>%
-    html_nodes(xpath = '//*[@id="table_active"]/tbody//td[1]/a') %>% 
-    html_attr("href")
-  players <- webpage %>%
-    html_nodes(xpath = '//*[@id="table_active"]/tbody//td[1]/a') %>% 
-    html_text()
-  if (length(players) == 0) {
-    return(data.frame())
-  }
-  tibble <- html_table(webpage)[[1]]
-  player_df <- data.frame(
-    "SpotracName" = players,
-    "SpotracUrl" = player_links
-  )
-  player_df <- player_df %>%
-    filter(player_df$SpotracName != "")
-  player_df <- data.frame(
-      "SpotracName" = player_df$SpotracName,
-      "SpotracUrl" = player_df$SpotracUrl,
-      "SpotracSalary" = tibble$`Cap Hit`,
-      "SpotracNotes" = "",
-      "SpotracSource" = link,
-      "SpotracSeason" = paste0(str_sub(link, -5, -2), "-", as.integer(str_sub(link, -5, -2))-2000+1),
-      "SpotracClean" = make_clean_names(str_replace_all(str_to_lower(player_df$SpotracName), "\\.", ""))
-    )
-  return(player_df)
+# Function to clean text
+clean_text <- function(text){
+  text_clean <- gsub(" ", "", gsub("\n", "", text))
+  return(text_clean)
 }
 
-spotrac_data <- {}
+# Function to scrape data from Spotrac links
+scrape_spotrac_links <- function(link){
+  webpage <- read_html(link)
+  
+  player <- webpage %>%
+    html_nodes(xpath = '//*[@id="table_active"]/tbody//td[1]/a') %>% 
+    html_text()
+  # If no players found, return empty data frame
+  if (length(player) == 0) {
+    return(data.frame())
+  }
+  # Clean and filter data
+  player <- player[player != ""]
+  player_clean <- make_clean_names(tolower(gsub("\\.", "", player)))
+  
+  player_link <- webpage %>%
+    html_nodes(xpath = '//*[@id="table_active"]/tbody//td[1]/a') %>% 
+    html_attr("href")
+  # Filter invalid links
+  player_link <- player_link[player_link != "javascript:void(0)"]
+  
+  position <- webpage %>%
+    html_nodes(xpath = '//*[@id="table_active"]/tbody//td[2]') %>% 
+    html_text() %>%
+    clean_text()
+  
+  age <- webpage %>%
+    html_nodes(xpath = '//*[@id="table_active"]/tbody//td[3]') %>% 
+    html_text() %>%
+    clean_text()
+  
+  type <- webpage %>%
+    html_nodes(xpath = '//*[@id="table_active"]/tbody//td[4]') %>% 
+    html_text() %>%
+    clean_text()
+  
+  cap_hit <- webpage %>%
+    html_nodes(xpath = '//*[@id="table_active"]/tbody//td[5]') %>% 
+    html_text() %>%
+    clean_text()
+  
+  # Build season and create data frame
+  season <- paste0(str_sub(link, -5, -2), "-", as.integer(str_sub(link, -5, -2))-2000+1)
+  contracts <- data.frame(player, player_link, season, cap_hit, position, age, type, link, player_clean)
+  # Create new column without suffixes like _sr, _jr, _ii, _iii, _iv
+  contracts <- contracts %>%
+    mutate(player_clean_no_suffix = gsub("_(sr|jr|ii|iii|iv|v|vi|vii)$", "", player_clean))
+  # Exclude "Two-Way" contracts
+  contracts <- contracts %>% 
+    filter(cap_hit != "Two-Way") %>%
+    filter(cap_hit != "-")
+  
+  return(contracts)
+}
+
+# Scrape data from all links
+spotrac_data <- list()
 
 for(i in 1:length(spotrac_links)){
   data <- scrape_spotrac_links(spotrac_links[i])
-  spotrac_data[i] <- list(data)
+  spotrac_data <- append(spotrac_data, list(data))
 }
 
+# Combine all data into one data frame and order by player and season
 spotrac_data_df <- do.call("rbind", spotrac_data)
-spotrac_data_df <- spotrac_data_df[order(spotrac_data_df$SpotracName, spotrac_data_df$SpotracSeason),]
+spotrac_data_df <- spotrac_data_df[order(spotrac_data_df$player, spotrac_data_df$season),]
 
+# Save to CSV
 write.csv(spotrac_data_df, "../data/Spotrac.csv", row.names = F)
