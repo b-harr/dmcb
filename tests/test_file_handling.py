a20587c1d7be7f1ff6ec3dac1c8711c8afb19d09
B. Harrison <cbharrison@utexas.edu>
2024-11-26 17:47:46 -0600

    Clear __init__

--- tests/test_file_handling.py
+++ tests/test_file_handling.py
@@ -6,7 +6,7 @@ project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 if project_root not in sys.path:
     sys.path.insert(0, project_root)
 
-from utils.file_handler import read_file, write_file
+from utils.csv_handler import read_file, write_file
 
 df = read_file("data", "contract_types.csv")
 write_file(df, "tests/data", "types.csv")
--- utils/__init__.py
+++ utils/__init__.py
@@ -1,3 +1,1 @@
 # utils/__init__.py
-
-#from .text_formatter import format_text
