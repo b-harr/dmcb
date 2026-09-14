dde82367b60e88ca717737b5ea57d29a46403fa4
B. Harrison <cbharrison@utexas.edu>
2026-09-14 14:57:38 -0500

    Misc updates

--- dmcb-dev/tests/test_save_website.py
+++ dmcb-dev/tests/test_save_website.py
@@ -9,18 +9,18 @@ from main import save_website
 
 
 if __name__ == "__main__":
-    team_url = "https://www.spotrac.com/nba/la-clippers/yearly"
-    output_path = os.path.join(project_root, "tests/cache/team.html")
+    team_url = "https://www.spotrac.com/nba/toronto-raptors/yearly"
+    output_path = os.path.join(os.path.dirname(__file__), "cache/team.html")
     save_website(team_url, output_path)
 
     player_url = "https://www.spotrac.com/nba/player/_/id/82196/victor-wembanyama"
-    output_path = os.path.join(project_root, "tests/cache/player.html")
+    output_path = os.path.join(os.path.dirname(__file__), "cache/player.html")
     save_website(player_url, output_path)
 
     sportsws_url = "https://sports.ws/nba/stats"
-    output_path = os.path.join(project_root, "tests/cache/sportsws.html")
+    output_path = os.path.join(os.path.dirname(__file__), "cache/sportsws.html")
     save_website(sportsws_url, output_path)
 
     bbref_url = "https://www.basketball-reference.com/leagues/NBA_2026_totals.html"
-    output_path = os.path.join(project_root, "tests/cache/bbref.html")
+    output_path = os.path.join(os.path.dirname(__file__), "cache/bbref.html")
     save_website(bbref_url, output_path)
